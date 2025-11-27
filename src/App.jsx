40ba951ed950b76da3329e3429de94b244c36548
jagnyadattadcs <jagnyadatta.dcs@gmail.com>
2025-11-27 13:00:44 +0530

    Just Checking repo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-red-500 text-2xl'>Sports Lover</h1>
+      <h1 className='text-red-500 text-2xl'>Sports Lover Frontend checking</h1>
     </>
   )
 }
